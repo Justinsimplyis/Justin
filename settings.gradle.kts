23f3fe7a1acc03a3487c89a7f62f04ecb96e7316
Justin Plaatjies <j.plaatjies08@gmail.com>
2024-12-02 10:51:24 +0200

    Initial Commit
    This is the First Commit

---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Role_Bridge_User-Admin_Connect"
+include(":app")
+ 
